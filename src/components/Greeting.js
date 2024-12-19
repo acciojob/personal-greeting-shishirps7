9781efbc7fb4518b9c843270e1e7ef5337a337df
shishirps7 <sizaleen77@gmail.com>
2024-12-19 16:52:54 +0530

    yes5

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,17 +1,26 @@
-import React,{useState} from "react";
-let Greeting=()=>{
-    let [name,setName]=useState("Hello na!")
-  function update(e){
-    let value = e.target.value 
-    setName(value)
-     
-  }
-  return (
-    <div>
-      <p>Enter your  name:</p>
-        <input type="text" onChange={update} value={name}/>    
-        <p>{name}</p>
+import React, { useState } from "react";
+
+let Greeting = () => {
+    let [inputValue, setInputValue] = useState("");
+    let [name, setName] = useState("Hello na!");
+
+    function update(e) {
+        let value = e.target.value;
+        setInputValue(value);
+    }
+
+    function greet() {
+        setName(`Hello ${inputValue}`);
+    }
+
+    return (
+        <div>
+            <p>Enter your name:</p>
+            <input type="text" onChange={update} value={inputValue} />
+            <button onClick={greet}>Greet</button>
+            <p>{name}</p>
         </div>
-  )
-}
-export default Greeting
+    );
+};
+
+export default Greeting;
